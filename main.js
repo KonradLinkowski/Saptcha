d90e746dce10f60f80417eedc8c26fffe9267010
Konrad Linkowski <linkowski.konrad97@gmail.com>
2019-08-15 22:14:28 +0200

    Tooltips and virify / skip button rename on selected

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 (() => {
   'use strict'
   const tilesContainer = document.querySelector('#tiles_container')
+  const verifyButton = document.querySelector('#verify_button')
+
   const selectTile = e => {
-    e.currentTarget.classList.toggle('tile--selected')
+    const el = e.currentTarget
+    selectedTiles[el.dataset.index] = el.classList.toggle('tile--selected')
+    verifyButton.textContent = selectedTiles.some(e => e) ? 'Verify' : 'Skip'
   }
+
   const createTile = (col, index) => {
     const wrapper = document.createElement('div')
     const img = document.createElement('div')
@@ -13,7 +18,7 @@
     wrapper.classList.add('tile')
     wrapper.setAttribute('tabindex', 0)
     wrapper.addEventListener('click', selectTile)
-    wrapper.dataset['index'] = index
+    wrapper.dataset.index = index
     return wrapper
   }
 
@@ -24,5 +29,8 @@
     const availableSizes = [3, 4]
     availableSizes.forEach(size => tilesContainer.classList.toggle(`tiles_container--${size}`, columnSize == size))
   }
-  renderGrid(4)
+
+  const columns = 4
+  const selectedTiles = Array(columns ** 2).fill(false)
+  renderGrid(columns)
 })()
