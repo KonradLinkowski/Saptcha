8b10a3120802a24177015e5dad623385122034e5
Konrad Linkowski <linkowski.konrad97@gmail.com>
2019-08-15 19:22:35 +0200

    Tile selection

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 (() => {
+  'use strict'
   const tilesContainer = document.querySelector('#tiles_container')
+  const selectTile = e => {
+    e.currentTarget.classList.toggle('tile--selected')
+  }
   const createTile = () => {
-    const img = document.createElement('img')
-    img.src = 'images/95.png'
-    img.classList.add('tile')
-    img.setAttribute('tabindex', 0)
-    return img
+    const wrapper = document.createElement('div')
+    const img = document.createElement('div')
+    wrapper.appendChild(img)
+    img.style.backgroundImage = 'url("images/95.png")'
+    img.classList.add('tile__image')
+    wrapper.classList.add('tile')
+    wrapper.classList.add('tile--4')
+    wrapper.setAttribute('tabindex', 0)
+    wrapper.addEventListener('click', selectTile)
+    return wrapper
   }
   const numberOfTiles = 16
   const tiles = Array(numberOfTiles).fill(0).map(createTile)
